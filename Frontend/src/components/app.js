af52b3ce935b1aebc69382f86f51afc6be98c178
Fabio Fernandes <fabio@fabiofernandes.net>
2016-09-14 12:29:00 -0500

    router configured

--- Frontend/src/components/app.js
+++ Frontend/src/components/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React up and Running...</div>
+      <div>{this.props.children}</div>
     );
   }
 }
---
+++ Frontend/src/components/kb-index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class KbIndex extends Component{
+
+	render(){
+		return(
+
+				<div>
+					<h1>Kb Index</h1>
+				</div>
+				
+			);
+	}
+
+}
+
+export default KbIndex;
